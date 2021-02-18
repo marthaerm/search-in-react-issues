92c8022cdc4d63a33d031029cde8a8495e9d3940
MarthaRM <rdz.marthaelena@gmail.com>
2021-02-18 17:56:46 -0600

    Search implemented, show results still missing

--- src/Search.js
+++ src/Search.js
@@ -1,22 +1,29 @@
-
 import React, { Component } from "react";
-
-
 
 class Search extends Component{
     constructor() {
         super()
         this.state = {
             isLoaded: false,
-            issues: []
+            issues: [],
+            issuesFound: []
         };
     }
-
-
+   
+    findWord(){
+        let { issues, issuesFound } = this.state;
+        let word = this.getTitle.value;
+        issuesFound = [];
+        issues.map(function (issue) {
+            if(issue.title.includes(word))
+                issuesFound.push(issue);
+        })
+        console.log(issuesFound);
+    }
+    
     componentDidMount(){
         fetch("https://api.github.com/repos/facebook/react/issues")
             .then(response => response.json())
-           // .then(issues => console.log(issues))
             .then(
                 (issues) => {
                     this.setState({
@@ -29,15 +36,23 @@ class Search extends Component{
     
 
     render() {
-        const { isLoaded, issues } = this.state;
+        const { isLoaded, issues, issuesFound } = this.state;
         if(!isLoaded)
             return (<div>loading..</div>) 
         else
             return(
                 <div>
-                    <input type="text" />
-                    <p>{issues[0].title}</p>
-                    {console.log(issues)}
+                    <input type="text" ref={(input) => (this.getTitle = input)}/>
+                    <button onClick={(e) => this.findWord()}>Search</button>
+                    
+                    {
+                        issuesFound.map(function (issue) {
+                            return(
+                            <p>{issue.title}</p>
+                            )
+                            //console.log(issue.title);
+                        })
+                    }
                 </div>
             );  
     }
